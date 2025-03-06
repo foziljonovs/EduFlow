9f233a789f32e8f220dd6566864517d72ce986bb
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-03-06 13:28:40 +0500

    Add user service interfaces and token management
    Introduce `ITokenService` for token generation and define
    `IStudentService`, `ITeacherService`, and `IUserService`
    interfaces for managing student, teacher, and user operations.
    These interfaces support CRUD operations, user authentication,
    and password management, organized under appropriate namespaces.

---
+++ EduFlow.BLL/Interfaces/Users/Student/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using EduFlow.BLL.DTOs.Users.Student;
+
+namespace EduFlow.BLL.Interfaces.Users.Student;
+
+public interface IStudentService
+{
+    Task<IEnumerable<StudentForResultDto>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<StudentForResultDto> GetByIdAsync(long id, CancellationToken cancellationToken = default);
+    Task<bool> AddAsync(StudentForCreateDto dto, CancellationToken cancellationToken = default);
+    Task<bool> UpdateAsync(long id, StudentForUpdateDto dto, CancellationToken cancellationToken = default);
+    Task<bool> DeleteAsync(long id, CancellationToken cancellationToken = default);
+    Task<StudentForResultDto> GetByPhoneNumberAsync(string phoneNumber, CancellationToken cancellationToken = default);
+}
---
+++ EduFlow.BLL/Interfaces/Users/User/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using EduFlow.BLL.DTOs.Users.User;
+
+namespace EduFlow.BLL.Interfaces.Users.User;
+
+public interface IUserService
+{
+    Task<IEnumerable<UserForResultDto>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<UserForResultDto> GetByIdAsync(long id, CancellationToken cancellationToken = default);
+    Task<bool> RegisterAsync(UserForCreateDto dto, CancellationToken cancellationToken = default);
+    Task<string> LoginAsync(UserForLoginDto dto, CancellationToken cancellationToken = default);
+    Task<bool> UpdateAsync(long id, UserForUpdateDto dto, CancellationToken cancellationToken = default);
+    Task<bool> DeleteAsync(long id, CancellationToken cancellationToken = default);
+    Task<bool> ChangePasswordAsync(long id, UserForChangePasswordDto dto, CancellationToken cancellationToken = default);
+    Task<bool> VerifyPasswordAsync(long id, string password, CancellationToken cancellationToken = default);
+}
