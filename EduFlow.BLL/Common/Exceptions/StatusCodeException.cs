f3ad8bd904a695a8b4a439fcee178ca30b084477
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-03-07 21:30:04 +0500

    Implement user-related operations in UserService
    Refactor `UserService.cs` to add logic for changing passwords, deleting users, retrieving users, logging in, registering, updating, and verifying passwords. Replace `NotImplementedException` with try-catch blocks for error handling. Update the constructor to accept `IUnitOfWork`, `IMapper`, and `ITokenService` as dependencies. Introduce `StatusCodeException` for improved error reporting with HTTP status codes.

---
+++ EduFlow.BLL/Common/Exceptions/StatusCodeException.cs
@@ -0,0 +1,8 @@
+﻿using System.Net;
+
+namespace EduFlow.BLL.Common.Exceptions;
+
+public class StatusCodeException(HttpStatusCode code, string message) : Exception(message)
+{
+    public HttpStatusCode StatusCode { get; } = code;
+}
