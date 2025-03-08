84e12f07885b48b495f0e6d4acbcc59cd45b5549
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-03-08 15:08:16 +0500

    Add Serilog logging and enhance error handling
    - Added Serilog package references to `EduFlow.BLL.csproj` and `EduFlow.WebApi.csproj`.
    - Modified `ITeacherService` to update method signature for `UpdateAsync`.
    - Implemented logging and error handling in `TeacherService` methods.
    - Enhanced `UserService` with logging and error handling across multiple methods.
    - Introduced `AddSerilogConfiguration` in `LayerConfiguration` for logging setup.
    - Updated `Program.cs` to configure Serilog and enable request logging middleware.

--- EduFlow.BLL/Interfaces/Users/ITeacherService.cs
+++ EduFlow.BLL/Interfaces/Users/ITeacherService.cs
@@ -7,6 +7,6 @@ public interface ITeacherService
     Task<IEnumerable<TeacherForResultDto>> GetAllAsync(CancellationToken cancellationToken = default);
     Task<TeacherForResultDto> GetByIdAsync(long id, CancellationToken cancellationToken = default);
     Task<bool> AddAsync(TeacherForCreateDto dto, CancellationToken cancellationToken = default);
-    Task<bool> UpdateAsync(TeacherForUpdateDto dto, CancellationToken cancellationToken = default);
+    Task<bool> UpdateAsync(long id, TeacherForUpdateDto dto, CancellationToken cancellationToken = default);
     Task<bool> DeleteAsync(long id, CancellationToken cancellationToken = default);
 }
--- EduFlow.BLL/Services/Users/TeacherService.cs
+++ EduFlow.BLL/Services/Users/TeacherService.cs
@@ -1,32 +1,129 @@
-﻿using EduFlow.BLL.DTOs.Users.Teacher;
+﻿using AutoMapper;
+using EduFlow.BLL.Common.Exceptions;
+using EduFlow.BLL.DTOs.Users.Teacher;
 using EduFlow.BLL.Interfaces.Users;
+using EduFlow.DAL.Interfaces;
+using EduFlow.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Net;
 
 namespace EduFlow.BLL.Services.Users;
 
-public class TeacherService : ITeacherService
+public class TeacherService(
+    IUnitOfWork unitOfWork,
+    IMapper mapper,
+    ILogger<TeacherService> logger) : ITeacherService
 {
-    public Task<bool> AddAsync(TeacherForCreateDto dto, CancellationToken cancellationToken = default)
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IMapper _mapper = mapper;
+    private readonly ILogger<TeacherService> _logger = logger;
+    public async Task<bool> AddAsync(TeacherForCreateDto dto, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var existsUser = await _unitOfWork.User.GetAsync(dto.UserId);
+            if (existsUser != null)
+                throw new StatusCodeException(HttpStatusCode.NotFound, "User not found.");
+
+            var existsTeacher = await _unitOfWork.Teacher.GetAllAsync().FirstOrDefaultAsync(x => x.UserId == dto.UserId);
+            if (existsTeacher is not null)
+                throw new StatusCodeException(HttpStatusCode.Conflict, "Teacher already exists.");
+
+            var savedTeacher = _mapper.Map<Teacher>(dto);
+            return await _unitOfWork.Teacher.AddConfirmAsync(savedTeacher);
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError($"An error occured while adding the teacher. {ex}");
+            throw;
+        }
     }
 
-    public Task<bool> DeleteAsync(long id, CancellationToken cancellationToken = default)
+    public async Task<bool> DeleteAsync(long id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var existsTeacher = await _unitOfWork.Teacher.GetAsync(id);
+            if (existsTeacher is null)
+                throw new StatusCodeException(HttpStatusCode.NotFound, "Teacher not found.");
+
+            existsTeacher.IsDeleted = true;
+            return await _unitOfWork.Teacher.UpdateAsync(existsTeacher);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"An error occured while delete the teacher. {ex}");
+            throw;
+        }
     }
 
-    public Task<IEnumerable<TeacherForResultDto>> GetAllAsync(CancellationToken cancellationToken = default)
+    public async Task<IEnumerable<TeacherForResultDto>> GetAllAsync(CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var teachers = await _unitOfWork.Teacher
+                .GetAllAsync()
+                .Where(x => x.IsDeleted == false)
+                .ToListAsync(cancellationToken);
+
+            if (!teachers.Any())
+                throw new StatusCodeException(HttpStatusCode.NotFound, "Teachers not found.");
+
+            return _mapper.Map<IEnumerable<TeacherForResultDto>>(teachers);
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError($"An error occured while get all the teacher. {ex}");
+            throw;
+        }
     }
 
-    public Task<TeacherForResultDto> GetByIdAsync(long id, CancellationToken cancellationToken = default)
+    public async Task<TeacherForResultDto> GetByIdAsync(long id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var teacher = await _unitOfWork.Teacher.GetAsync(id);
+            if (teacher is null)
+                throw new StatusCodeException(HttpStatusCode.NotFound, "Teacher not found.");
+
+            if (teacher.IsDeleted)
+                throw new StatusCodeException(HttpStatusCode.Gone, "This teacher has been deleted.");
+
+            return _mapper.Map<TeacherForResultDto>(teacher);
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError($"An error occured while get by id the teacher. {ex}");
+            throw;
+        }
     }
 
-    public Task<bool> UpdateAsync(TeacherForUpdateDto dto, CancellationToken cancellationToken = default)
+    public async Task<bool> UpdateAsync(long id, TeacherForUpdateDto dto, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var existsTeacher = await _unitOfWork.Teacher.GetAsync(id);
+            if (existsTeacher is null)
+                throw new StatusCodeException(HttpStatusCode.NotFound, "Teacher not found.");
+
+            if (existsTeacher.IsDeleted)
+                throw new StatusCodeException(HttpStatusCode.Gone, "This teacher has been deleted.");
+
+            var existsUser = await _unitOfWork.User.GetAsync(dto.UserId);
+            if (existsUser is null)
+                throw new StatusCodeException(HttpStatusCode.NotFound, "User not found.");
+
+            var updateTeacher = _mapper.Map<Teacher>(dto);
+            updateTeacher.Id = id;
+            updateTeacher.UpdatedAt = DateTime.UtcNow.AddHours(5);
+
+            return await _unitOfWork.Teacher.UpdateAsync(updateTeacher);
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError($"An error occured while update the teacher. {ex}");
+            throw;
+        }
     }
 }
