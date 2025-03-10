7f2b9c8c30ca0798881ea8ba22963191682b3d55
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-03-10 13:34:23 +0500

    Refactor project structure and add user controllers
    Updated `EduFlow.WebApi.csproj` to organize controllers into specific folders. Added `AuthController` for authentication with a `LoginAsync` method. Introduced `UserController` for user management, including methods for CRUD operations and password handling, with comprehensive error handling.

---
+++ EduFlow.WebApi/Controllers/Common/Users/UserController.cs
@@ -0,0 +1,150 @@
+﻿using EduFlow.BLL.Common.Exceptions;
+using EduFlow.BLL.DTOs.Users.User;
+using EduFlow.BLL.Interfaces.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace EduFlow.WebApi.Controllers.Common.Users;
+
+[Route("api/users")]
+[ApiController, Authorize]
+public class UserController(
+    IUserService service) : ControllerBase
+{
+    private readonly IUserService _service = service;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var response = await _service.GetAllAsync(cancellationToken);
+            return Ok(response);
+        }
+        catch(StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpGet("{id:long}")]
+    public async Task<IActionResult> GetByIdAsync([FromRoute] long id, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.GetByIdAsync(id, cancellation);
+            return Ok(response);
+        }
+        catch(StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegisterAsync([FromBody] UserForCreateDto dto, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.RegisterAsync(dto, cancellation);
+            return Ok(response);
+        }
+        catch(ValidationException ex)
+        {
+            return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+        }
+        catch(StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPut("{id:long}")]
+    public async Task<IActionResult> UpdateAsync([FromRoute] long id, [FromBody] UserForUpdateDto dto, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.UpdateAsync(id, dto, cancellation);
+            return Ok(response);
+        }
+        catch (ValidationException ex)
+        {
+            return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+        }
+        catch(StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpDelete("{id:long}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute] long id, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var repsonse = await _service.DeleteAsync(id, cancellation);
+            return Ok(response);
+        }
+        catch(StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpGet("{id:long}/verify")]
+    public async Task<IActionResult> VerifyAsync([FromRoute] long id, string password, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.VerifyPasswordAsync(id, password, cancellation);
+            return Ok(response);
+        }
+        catch(StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPost("{id:long}/change-password")]
+    public async Task<IActionResult> ChangePasswordAsync([FromRoute] long id, [FromBody] UserForChangePasswordDto dto, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.ChangePasswordAsync(id, dto, cancellation);
+            return Ok(response);
+        }
+        catch(StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+}
