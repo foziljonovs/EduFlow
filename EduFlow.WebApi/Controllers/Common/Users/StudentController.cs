e83efd1110b698717062a7ab8452aba995180460
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-03-10 20:02:23 +0500

    Add StudentController for student management operations
    Implemented the StudentController in the EduFlow.WebApi.Controllers.Common.Users namespace. This controller handles HTTP requests for student-related actions, including retrieval, addition, updating, and deletion of student records. It leverages the IStudentService for asynchronous operations and includes comprehensive error handling to return appropriate HTTP status codes and messages.

---
+++ EduFlow.WebApi/Controllers/Common/Users/StudentController.cs
@@ -0,0 +1,132 @@
+﻿using EduFlow.BLL.Common.Exceptions;
+using EduFlow.BLL.DTOs.Users.Student;
+using EduFlow.BLL.Interfaces.Users;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduFlow.WebApi.Controllers.Common.Users;
+
+[Route("api/students")]
+[ApiController, Authorize]
+public class StudentController(
+    IStudentService service) : ControllerBase
+{
+    private readonly IStudentService _service = service;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var response = await _service.GetAllAsync(cancellationToken);
+            return Ok(response);
+        }
+        catch(StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpGet("id:long")]
+    public async Task<IActionResult> GetByIdAsync([FromRoute] long id, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.GetByIdAsync(id, cancellation);
+            return Ok(response);
+        }
+        catch (StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddAsync([FromBody] StudentForCreateDto dto, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.AddAsync(dto, cancellation);
+            return Ok(response);
+        }
+        catch(ValidationException ex)
+        {
+            return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+        }
+        catch(StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpDelete("{id:long}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute] long id, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.DeleteAsync(id, cancellation);
+            return Ok(response);
+        }
+        catch(StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPut("{id:long}")]
+    public async Task<IActionResult> UpdateAsync([FromRoute] long id, [FromBody] StudentForUpdateDto dto, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.UpdateAsync(id, dto, cancellation);
+            return Ok(response);
+        }
+        catch(ValidationException ex)
+        {
+            return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+        }
+        catch(StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpGet("{phoneNumber:string}")]
+    public async Task<IActionResult> GetAllByPhoneNumberAsync([FromRoute] string phoneNumber, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.GetByPhoneNumberAsync(phoneNumber, cancellation);
+            return Ok(response);
+        }
+        catch(StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+}
