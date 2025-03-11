1f52a7aed40f8a7fdeaec0108bda6acef1b30682
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-03-11 22:35:54 +0500

    Update password handling and validation logic
    - Changed `Password` property in `User` class to type `text` for longer passwords.
    - Enhanced password hashing in `PasswordHelper` using `BCrypt.Net` for better security.
    - Inverted user existence check in `UserService` to throw an exception if user exists.
    - Updated `AppDbContextModelSnapshot` to reflect changes in the `User` entity.
    - Added migration class `UpdateUserPassword` to update database schema for `Password`.
    - Updated migration designer file to ensure model builder reflects current schema.

---
+++ EduFLow.DAL/Migrations/20250311171217_UpdateUserPassword.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduFlow.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(8)",
+                oldMaxLength: 8,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "Users",
+                type: "character varying(8)",
+                maxLength: 8,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
--- EduFlow.BLL/Common/Security/PasswordHelper.cs
+++ EduFlow.BLL/Common/Security/PasswordHelper.cs
@@ -4,7 +4,7 @@ public static class PasswordHelper
 {
     public static (string Hash, string Salt) Hash(string password)
     {
-        string salt = Guid.NewGuid().ToString();
+        string salt = BCrypt.Net.BCrypt.GenerateSalt();
         string hash = BCrypt.Net.BCrypt.HashPassword(password, salt);
 
         return (Hash: hash, Salt: salt);
@@ -12,6 +12,6 @@ public static class PasswordHelper
 
     public static bool Verify(string password, string hash, string salt)
     {
-        return BCrypt.Net.BCrypt.Verify(password + salt, hash);
+        return BCrypt.Net.BCrypt.Verify(password, hash);
     }
 }
