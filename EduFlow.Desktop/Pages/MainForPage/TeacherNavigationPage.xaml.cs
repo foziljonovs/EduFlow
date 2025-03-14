cc6062a6e4670bb59020e806fc6954964968f161
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-03-14 05:51:05 +0500

    Refactor UI and navigation structure
    - Updated `MainPage.xaml` to improve layout with a `Grid`.
    - Replaced `StackPanel` with a `Grid` in `MainWindow.xaml` and added a `Frame` for navigation.
    - Removed radio button click handlers in `MainWindow.xaml.cs` and introduced a centralized `NavigatePage(Page page)` method.
    - Changed initial page loading to `TeacherNavigationPage`.
    - Added new `CoursePage.xaml` and `ManagerNavigationPage.xaml` for course management.
    - Introduced `TeacherNavigationPage.xaml` for navigation between main and course pages.
    - Overall, these changes enhance modularity and organization in the UI and navigation logic.

---
+++ EduFlow.Desktop/Pages/MainForPage/TeacherNavigationPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using EduFlow.Desktop.Pages.CourseForPages;
+using EduFlow.Desktop.Windows;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EduFlow.Desktop.Pages.MainForPage;
+
+/// <summary>
+/// Interaction logic for TeacherNavigationPage.xaml
+/// </summary>
+public partial class TeacherNavigationPage : Page
+{
+    public TeacherNavigationPage()
+    {
+        InitializeComponent();
+    }
+
+    private MainWindow getWindow()
+        => Window.GetWindow(this) as MainWindow;    
+    private void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+        GetMainPage();
+    }
+
+    private void GetMainPage()
+    {
+        MainPage page = new MainPage();
+        var window = getWindow();
+        if (window != null)
+            window.NavigatePage(page);
+    }
+
+    private void MainButton_Click(object sender, RoutedEventArgs e)
+    {
+        GetMainPage();
+    }
+
+    private void CoursesButton_Click(object sender, RoutedEventArgs e)
+    {
+        CoursePage page = new CoursePage();
+        var window = getWindow();
+        if(window != null)
+            window.NavigatePage(page);
+    }
+}
