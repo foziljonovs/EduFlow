800dd452873fe6a22786270847aa323c47512477
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-03-16 12:45:56 +0500

    Remove Class1 and implement authentication services
    - Removed `Class1.cs` as it is no longer needed.
    - Added "Services" folder and project references to `EduFlow.BLL` and `EduFlow.Domain`.
    - Introduced `AuthApi.cs` for centralized API endpoint URLs.
    - Added `IdentitySingelton.cs` to manage user identity with a singleton pattern.
    - Created `TokenHandler.cs` to parse JWT tokens and populate identity information.
    - Defined `IAuthServer.cs` interface for authentication services.
    - Implemented `AuthServer.cs` to handle login requests using `IHttpClientFactory`.

---
+++ EduFlow.Desktop.Integrated/Security/IdentitySingelton.cs
@@ -0,0 +1,31 @@
+﻿using EduFlow.Domain.Enums;
+
+namespace EduFlow.Desktop.Integrated.Security;
+
+public class IdentitySingelton
+{
+    public static IdentitySingelton identitySingelton;
+
+    public static IdentitySingelton GetInstance()
+    {
+        if (identitySingelton == null)
+            identitySingelton = new IdentitySingelton();
+
+        return identitySingelton;
+    }
+
+    public string Token { get; set; } = string.Empty;
+    public long Id { get; set; }
+    public string Fullname { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public UserRole Role { get; set; }
+
+    public void Reset()
+    {
+        Token = string.Empty;
+        Id = 0;
+        Fullname = string.Empty;
+        PhoneNumber = string.Empty;
+        Role = UserRole.Administrator;
+    }
+}
---
+++ EduFlow.Desktop.Integrated/Servers/Repositories/Auth/AuthServer.cs
@@ -0,0 +1,40 @@
+﻿using EduFlow.BLL.DTOs.Users.User;
+using EduFlow.Desktop.Integrated.Api.Auth;
+using EduFlow.Desktop.Integrated.Servers.Interfaces.Auth;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace EduFlow.Desktop.Integrated.Servers.Repositories.Auth;
+
+public class AuthServer(IHttpClientFactory httpClientFactory) : IAuthServer
+{
+    private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+    public async Task<(bool result, string token)> LoginAsync(UserForLoginDto dto)
+    {
+        try
+        {
+            using var client = _httpClientFactory.CreateClient();
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, $"{AuthApi.LOGIN_URL}")
+            {
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(dto),
+                    Encoding.UTF8,
+                    "application/json")
+            };
+
+            var response = await client.SendAsync(requestMessage);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string token = await response.Content.ReadAsStringAsync();
+                return (result: true, token: token);
+            }
+            
+            return (result: false, token: string.Empty);
+        }
+        catch(Exception ex)
+        {
+            return (result: false, token: string.Empty);
+        }
+    }
+}
