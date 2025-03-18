6d7502ae344fd68b03a2e683d688f870b1e2994c
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-03-18 21:49:27 +0500

    Add user service interface and implementation
    Updated project file to include Services\Courses\.
    Created IUserService interface for user management with
    async methods for CRUD operations and password handling.
    Implemented UserService class with a constructor
    initializing IUserServer; methods currently throw
    NotImplementedException.

---
+++ EduFlow.Desktop.Integrated/Services/Users/User/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using EduFlow.BLL.DTOs.Users.User;
+using EduFlow.Desktop.Integrated.Servers.Interfaces.Users.User;
+using EduFlow.Desktop.Integrated.Servers.Repositories.Users.User;
+using EduFlow.Desktop.Integrated.Services.Users.User.Interfaces;
+
+namespace EduFlow.Desktop.Integrated.Services.Users.User.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserServer _server;
+    public UserService()
+    {
+        this._server = new UserServer();
+    }
+    public Task<bool> ChangePasswordAsync(long id, UserForChangePasswordDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<UserForResultDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserForResultDto> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> RegisterAsync(UserForCreateDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(long id, UserForUpdateDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> VerifyPasswordAsync(long id, string password)
+    {
+        throw new NotImplementedException();
+    }
+}
