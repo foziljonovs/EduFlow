393cd67d4b029c38e3c6f4adac7a22ab05fa1576
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-03-19 17:04:34 +0500

    Add student management services and interface
    Updated project structure to include user service folders for Student and Teacher. Created IStudentServer interface for managing student data with methods for CRUD operations. Implemented StudentServer class to handle API interactions for student management, including authentication and JSON processing.

---
+++ EduFlow.Desktop.Integrated/Servers/Interfaces/Users/Student/IStudentServer.cs
@@ -0,0 +1,12 @@
+﻿using EduFlow.BLL.DTOs.Users.Teacher;
+
+namespace EduFlow.Desktop.Integrated.Servers.Interfaces.Users.Student;
+
+public interface IStudentServer
+{
+    Task<List<TeacherForResultDto>> GetAllAsync();
+    Task<TeacherForResultDto> GetByIdAsync(long id);
+    Task<bool> AddAsync(TeacherForCreateDto dto);
+    Task<bool> UpdateAsync(long id, TeacherForUpdateDto dto);
+    Task<bool> DeleteAsync(long id);
+}
---
+++ EduFlow.Desktop.Integrated/Servers/Repositories/Users/Student/StudentServer.cs
@@ -0,0 +1,144 @@
+﻿using EduFlow.BLL.DTOs.Users.Student;
+using EduFlow.BLL.DTOs.Users.Teacher;
+using EduFlow.Desktop.Integrated.Security;
+using EduFlow.Desktop.Integrated.Servers.Interfaces.Users.Student;
+using Newtonsoft.Json;
+using System.Data.Common;
+
+namespace EduFlow.Desktop.Integrated.Servers.Repositories.Users.Student;
+
+public class StudentServer : IStudentServer
+{
+    public Task<bool> AddAsync(TeacherForCreateDto dto)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/students");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var request = new HttpRequestMessage(HttpMethod.Post, client.BaseAddress)
+            {
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(dto),
+                    Encoding.UTF8,
+                    "application/json")
+            };
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+        catch(Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public Task<bool> DeleteAsync(long id)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/students/{id}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.DeleteAsync(client.BaseAddress);
+
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public Task<List<TeacherForResultDto>> GetAllAsync()
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/students");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync(client.BaseAddress);
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            List<StudentForResultDto> students = JsonConvert.DeserializeObject<List<StudentForResultDto>>(result)!;
+
+            return students;
+        }
+        catch(Exception ex)
+        {
+            return new List<StudentForResultDto>();
+        }
+    }
+
+    public Task<TeacherForResultDto> GetByIdAsync(long id)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/students/{id}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync(client.BaseAddress);
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            StudentForResultDto student = JsonConvert.DeserializeObject<StudentForResultDto>(result)!;
+
+            return student;
+        }
+        catch(Exception ex)
+        {
+            return new StudentForResultDto();
+        }
+    }
+
+    public Task<bool> UpdateAsync(long id, TeacherForUpdateDto dto)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/students/{id}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var request = new HttpRequestMessage(HttpMethod.Put, client.BaseAddress)
+            {
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(dto),
+                    Encoding.UTF8,
+                    "application/json")
+            };
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+        catch(Exception ex)
+        {
+            return false;
+        }
+    }
+}
