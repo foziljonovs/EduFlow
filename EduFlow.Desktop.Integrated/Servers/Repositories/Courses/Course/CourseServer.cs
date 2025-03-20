3424a9d161a7868d4de97c8e6cc092d67c9b2cbc
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-03-20 23:30:54 +0500

    Refactor course management structure and APIs
    - Removed folder references in `EduFlow.Desktop.Integrated.csproj`.
    - Added `ICourseServer` interface for course management operations.
    - Implemented `CourseServer` class to handle API interactions for courses.
    - Introduced `CourseService` class to serve as a higher-level service layer.
    - Added `ICourseService` interface for service-level course management methods.
    - Overall, these changes enhance the structure and clarity of course management in the application.

---
+++ EduFlow.Desktop.Integrated/Servers/Repositories/Courses/Course/CourseServer.cs
@@ -0,0 +1,194 @@
+﻿using EduFlow.BLL.DTOs.Courses.Course;
+using EduFlow.BLL.DTOs.Users.User;
+using EduFlow.Desktop.Integrated.Api.Auth;
+using EduFlow.Desktop.Integrated.Security;
+using EduFlow.Desktop.Integrated.Servers.Interfaces.Courses.Course;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace EduFlow.Desktop.Integrated.Servers.Repositories.Courses.Course;
+
+public class CourseServer : ICourseServer
+{
+    public async Task<bool> AddAsync(CourseForCreateDto dto)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/courses");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var request = new HttpRequestMessage(HttpMethod.Post, client.BaseAddress)
+            {
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(dto),
+                    Encoding.UTF8,
+                    "application/json")
+            };
+
+            var response = await client.SendAsync(request);
+
+            if(response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+        catch(Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> DeleteAsync(long id)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/courses/{id}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.DeleteAsync(client.BaseAddress);
+
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public async Task<List<CourseForResultDto>> GetAllAsync()
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/courses");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync(client.BaseAddress);
+            
+            var result = await response.Content.ReadAsStringAsync();
+
+            List<CourseForResultDto> courses = JsonConvert.DeserializeObject<List<CourseForResultDto>>(result)!;
+
+            return courses;
+        }
+        catch (Exception ex)
+        {
+            return new List<CourseForResultDto>();
+        }
+    }
+
+    public async Task<List<CourseForResultDto>> GetAllByCategoryIdAsync(long categoryId)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/courses/{categoryId}/category");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync(client.BaseAddress);
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            List<CourseForResultDto> courses = JsonConvert.DeserializeObject<List<CourseForResultDto>>(result)!;
+
+            return courses;
+        }
+        catch (Exception ex)
+        {
+            return new List<CourseForResultDto>();
+        }
+    }
+
+    public async Task<List<CourseForResultDto>> GetAllByTeacherIdAsync(long teacherId)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/courses/{teacherId}/teacher");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync(client.BaseAddress);
+
+            var result = await response.Content.ReadAsStringAsync();
+            
+            List<CourseForResultDto> courses = JsonConvert.DeserializeObject<List<CourseForResultDto>>(result)!;
+            
+            return courses;
+        }
+        catch (Exception ex)
+        {
+            return new List<CourseForResultDto>();
+        }
+    }
+
+    public async Task<CourseForResultDto> GetByIdAsync(long id)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/courses/{id}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync(client.BaseAddress);
+            
+            var result = await response.Content.ReadAsStringAsync();
+            
+            CourseForResultDto course = JsonConvert.DeserializeObject<CourseForResultDto>(result)!;
+            
+            return course;
+        }
+        catch (Exception ex)
+        {
+            return new CourseForResultDto();
+        }
+    }
+
+    public async Task<bool> UpdateAsync(long id, CourseForUpdateDto dto)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/courses/{id}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            
+            var request = new HttpRequestMessage(HttpMethod.Put, client.BaseAddress)
+            {
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(dto),
+                    Encoding.UTF8,
+                    "application/json")
+            };
+            
+            var response = await client.SendAsync(request);
+            
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+}
---
+++ EduFlow.Desktop.Integrated/Services/Courses/Course/CourseService.cs
@@ -0,0 +1,104 @@
+﻿using EduFlow.BLL.DTOs.Courses.Course;
+using EduFlow.Desktop.Integrated.Servers.Interfaces.Courses.Course;
+using EduFlow.Desktop.Integrated.Servers.Repositories.Courses.Course;
+
+namespace EduFlow.Desktop.Integrated.Services.Courses.Course;
+
+public class CourseService : ICourseService
+{
+    private readonly ICourseServer _server;
+    public CourseService()
+    {
+        this._server  = new CourseServer();
+    }
+    public async Task<bool> AddAsync(CourseForCreateDto dto)
+    {
+        try
+        {
+            var res = await _server.AddAsync(dto);
+            return res;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> DeleteAsync(long id)
+    {
+        try
+        {
+            var res = await _server.DeleteAsync(id);
+            return res;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public async Task<List<CourseForResultDto>> GetAllAsync()
+    {
+        try
+        {
+            var res = await _server.GetAllAsync();
+            return res;
+        }
+        catch (Exception ex)
+        {
+            return new List<CourseForResultDto>();
+        }
+    }
+
+    public async Task<List<CourseForResultDto>> GetAllByCategoryIdAsync(long categoryId)
+    {
+        try
+        {
+            var res = await _server.GetAllByCategoryIdAsync(categoryId);
+            return res;
+        }
+        catch (Exception ex)
+        {
+            return new List<CourseForResultDto>();
+        }
+    }
+
+    public async Task<List<CourseForResultDto>> GetAllByTeacherIdAsync(long teacherId)
+    {
+        try
+        {
+            var res = await _server.GetAllByTeacherIdAsync(teacherId);
+            return res;
+        }
+        catch (Exception ex)
+        {
+            return new List<CourseForResultDto>();
+        }
+    }
+
+    public async Task<CourseForResultDto> GetByIdAsync(long id)
+    {
+        try
+        {
+            var res = await _server.GetByIdAsync(id);
+            return res;
+        }
+        catch (Exception ex)
+        {
+            return new CourseForResultDto();
+        }
+    }
+
+    public async Task<bool> UpdateAsync(long id, CourseForUpdateDto dto)
+    {
+        try
+        {
+            var res = await _server.UpdateAsync(id, dto);
+            return res;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+}
