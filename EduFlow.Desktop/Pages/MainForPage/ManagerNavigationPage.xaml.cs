b51f59ec066f6c62274fe19be80e131707d8a14a
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-03-22 10:48:37 +0500

    Update navigation and identity handling in the app
    - Refactored `TokenHandler` to use standardized claim URIs.
    - Added `Loaded` event and click handlers in `ManagerNavigationPage.xaml`.
    - Restructured `ManagerNavigationPage.xaml.cs` for improved navigation and code clarity.
    - Implemented consistent button click handling in `TeacherNavigationPage.xaml.cs`.

--- EduFlow.Desktop/Pages/MainForPage/ManagerNavigationPage.xaml.cs
+++ EduFlow.Desktop/Pages/MainForPage/ManagerNavigationPage.xaml.cs
@@ -1,28 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EduFlow.Desktop.Pages.CourseForPages;
+using EduFlow.Desktop.Windows;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace EduFlow.Desktop.Pages.MainForPage
+namespace EduFlow.Desktop.Pages.MainForPage;
+
+/// <summary>
+/// Interaction logic for ManagerNavigationPage.xaml
+/// </summary>
+public partial class ManagerNavigationPage : Page
 {
-    /// <summary>
-    /// Interaction logic for ManagerNavigationPage.xaml
-    /// </summary>
-    public partial class ManagerNavigationPage : Page
+    public ManagerNavigationPage()
     {
-        public ManagerNavigationPage()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    private MainWindow GetMainWindow()
+        => Window.GetWindow(this) as MainWindow;
+
+    private void GetMainPage()
+    {
+        MainButton.IsChecked = true;
+        MainPage page = new MainPage();
+        var window = GetMainWindow();
+        if(window != null)
+            window.NavigatePage(page);
+    }
+    private void MainButton_Click(object sender, RoutedEventArgs e)
+    {
+        GetMainPage();
+    }
+
+    private void CoursesButton_Click(object sender, RoutedEventArgs e)
+    {
+        CoursesButton.IsChecked = true;
+        CoursePage page = new CoursePage();
+        var window = GetMainWindow(); 
+        if(window != null)
+            window.NavigatePage(page);
+    }
+
+    private void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+        GetMainPage();
     }
 }
