149c24a2a502b4e2a451e891b3b5feaaea2a5b8b
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-03-22 23:16:10 +0500

    Add GetAllFullInformationAsync methods to repositories
    Introduce asynchronous methods in ICourseRepository, IStudentRepository, and ITeacherRepository to fetch full information for courses, students, and teachers, including related entities. Implement these methods in CourseRepository, StudentRepository, and TeacherRepository using Entity Framework Core. Add private fields for AppDbContext and entity sets, and update constructors for proper initialization.

--- EduFLow.DAL/Interfaces/Users/IStudentRepository.cs
+++ EduFLow.DAL/Interfaces/Users/IStudentRepository.cs
@@ -4,4 +4,5 @@ namespace EduFlow.DAL.Interfaces.Users;
 
 public interface IStudentRepository : IRepository<Student>
 {
+    Task<List<Student>> GetAllFullInformationAsync();
 }
--- EduFLow.DAL/Interfaces/Users/ITeacherRepository.cs
+++ EduFLow.DAL/Interfaces/Users/ITeacherRepository.cs
@@ -4,4 +4,5 @@ namespace EduFlow.DAL.Interfaces.Users;
 
 public interface ITeacherRepository : IRepository<Teacher>
 {
+    Task<List<Teacher>> GetAllFullInformationAsync();
 }
