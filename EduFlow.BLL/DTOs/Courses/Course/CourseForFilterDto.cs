61c732b48fd16816deb5114a6e2d9f11b868e769
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-03-24 13:51:53 +0500

    Add course filtering functionality to CourseService
    - Updated ICourseService to include FilterAsync method.
    - Implemented FilterAsync in CourseService to filter courses by date, category, and teacher.
    - Added CourseForFilterDto class for filtering criteria.

---
+++ EduFlow.BLL/DTOs/Courses/Course/CourseForFilterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EduFlow.BLL.DTOs.Courses.Course;
+
+public class CourseForFilterDto
+{
+    public DateTime? StartedDate { get; set; }
+    public DateTime? FinishedDate { get; set; }
+    public long? CategoryId { get; set; }
+    public long? TeacherId { get; set; }
+}
