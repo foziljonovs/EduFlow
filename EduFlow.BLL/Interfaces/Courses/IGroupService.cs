a8a6bd3ed72cb76172e670972ce4331f44018ff6
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-04-06 23:29:45 +0500

    Refactor IGroupService and GroupService implementations
    Updated IGroupService to return IEnumerable instead of List for better flexibility. Enhanced GroupService methods with error handling and logging, including checks for group and course existence. Improved parameter naming for consistency and overall code maintainability.

--- EduFlow.BLL/Interfaces/Courses/IGroupService.cs
+++ EduFlow.BLL/Interfaces/Courses/IGroupService.cs
@@ -4,11 +4,11 @@ namespace EduFlow.BLL.Interfaces.Courses;
 
 public interface IGroupService
 {
-    Task<List<GroupForResultDto>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<IEnumerable<GroupForResultDto>> GetAllAsync(CancellationToken cancellationToken = default);
     Task<GroupForResultDto> GetByIdAsync(long id, CancellationToken cancellationToken = default);
-    Task<bool> AddAsync(GroupForCreateDto groupForCreationDto, CancellationToken cancellationToken = default);
-    Task<bool> UpdateAsync(long id, GroupForUpdateDto groupForUpdateDto, CancellationToken cancellationToken = default);
+    Task<bool> AddAsync(GroupForCreateDto dto, CancellationToken cancellationToken = default);
+    Task<bool> UpdateAsync(long id, GroupForUpdateDto dto, CancellationToken cancellationToken = default);
     Task<bool> DeleteAsync(long id, CancellationToken cancellationToken = default);
-    Task<List<GroupForResultDto>> GetAllByCourseIdAsync(long courseId, CancellationToken cancellationToken = default);
-    Task<List<GroupForResultDto>> FilterAsync(GroupForFilterDto dto, CancellationToken cancellationToken = default);
+    Task<IEnumerable<GroupForResultDto>> GetAllByCourseIdAsync(long courseId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<GroupForResultDto>> FilterAsync(GroupForFilterDto dto, CancellationToken cancellationToken = default);
 }
