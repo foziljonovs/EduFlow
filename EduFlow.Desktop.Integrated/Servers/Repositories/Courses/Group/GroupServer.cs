2fc48e3744ab9c86a54714d31cc06fa7cddd29a1
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-04-08 23:08:55 +0500

    Add IGroupServer interface and GroupServer implementation
    Introduces the `IGroupServer` interface with methods for managing groups, including CRUD operations and filtering. Implements the `GroupServer` class to interact with a web API using `HttpClient`, handling authentication and error management effectively.

---
+++ EduFlow.Desktop.Integrated/Servers/Repositories/Courses/Group/GroupServer.cs
@@ -0,0 +1,202 @@
+﻿using EduFlow.BLL.DTOs.Courses.Group;
+using EduFlow.Desktop.Integrated.Api.Auth;
+using EduFlow.Desktop.Integrated.Security;
+using EduFlow.Desktop.Integrated.Servers.Interfaces.Courses.Group;
+using Microsoft.AspNetCore.Authentication;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace EduFlow.Desktop.Integrated.Servers.Repositories.Courses.Group;
+
+public class GroupServer : IGroupServer
+{
+    public async Task<bool> AddAsync(GroupForCreateDto dto)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/groups");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var request = new HttpRequestMessage(HttpMethod.Post, client.BaseAddress)
+            {
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(dto),
+                    Encoding.UTF8,
+                    "application/json")
+            };
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+        catch(Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> DeleteAsync(long id)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/groups/{id}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.DeleteAsync(client.BaseAddress);
+
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public async Task<List<GroupForResultDto>> FilterAsync(GroupForFilterDto dto)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/groups/filter");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var request = new HttpRequestMessage(HttpMethod.Post, client.BaseAddress)
+            {
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(dto),
+                    Encoding.UTF8,
+                    "application/json")
+            };
+
+            var response = await client.SendAsync(request);
+            
+            var result = await response.Content.ReadAsStringAsync();
+
+            List<GroupForResultDto> groups = JsonConvert.DeserializeObject<List<GroupForResultDto>>(result)!;
+
+            return groups;
+        }
+        catch (Exception ex)
+        {
+            return new List<GroupForResultDto>();
+        }
+    }
+
+    public async Task<List<GroupForResultDto>> GetAllAsync()
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/groups");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync(client.BaseAddress);
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            List<GroupForResultDto> groups = JsonConvert.DeserializeObject<List<GroupForResultDto>>(result)!;
+
+            return groups;
+        }
+        catch (Exception ex)
+        {
+            return new List<GroupForResultDto>();
+        }
+    }
+
+    public async Task<List<GroupForResultDto>> GetAllByCourseIdAsync(long courseId)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/groups/{courseId}/course");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync(client.BaseAddress);
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            List<GroupForResultDto> groups = JsonConvert.DeserializeObject<List<GroupForResultDto>>(result)!;
+
+            return groups;
+        }
+        catch(Exception ex)
+        {
+            return new List<GroupForResultDto>();
+        }
+    }
+
+    public async Task<GroupForResultDto> GetByIdAsync(long id)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/groups/{id}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync(client.BaseAddress);
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            GroupForResultDto group = JsonConvert.DeserializeObject<GroupForResultDto>(result)!;
+
+            return group;
+        }
+        catch (Exception ex)
+        {
+            return new GroupForResultDto();
+        }
+    }
+
+    public async Task<bool> UpdateAsync(long id, GroupForUpdateDto dto)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            var token = IdentitySingelton.GetInstance().Token;
+
+            client.BaseAddress = new Uri($"{AuthApi.BASE_URL}/api/groups/{id}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var request = new HttpRequestMessage(HttpMethod.Put, client.BaseAddress)
+            {
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(dto),
+                    Encoding.UTF8,
+                    "application/json")
+            };
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+}
