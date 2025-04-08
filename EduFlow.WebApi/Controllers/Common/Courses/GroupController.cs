2409650511e5d4ed8bf3557e6fbcb54583519b17
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-04-08 22:11:54 +0500

    Add GroupController for managing groups via API
    Implemented a new `GroupController` class that provides RESTful endpoints for retrieving, adding, updating, and deleting groups. The controller integrates with `IGroupService` for business logic and includes error handling to return appropriate HTTP status codes and messages.

---
+++ EduFlow.WebApi/Controllers/Common/Courses/GroupController.cs
@@ -0,0 +1,150 @@
+﻿using EduFlow.BLL.Common.Exceptions;
+using EduFlow.BLL.DTOs.Courses.Group;
+using EduFlow.BLL.Interfaces.Courses;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduFlow.WebApi.Controllers.Common.Courses;
+
+[Route("api/groups")]
+[ApiController, Authorize]
+public class GroupController(
+    IGroupService service) : ControllerBase
+{
+    private readonly IGroupService _service = service;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.GetAllAsync(cancellation);
+            return Ok(response);
+        }
+        catch (StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpGet("{id:long}")]
+    public async Task<IActionResult> GetByIdAsync([FromRoute] long id, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.GetByIdAsync(id, cancellation);
+            return Ok(response);
+        }
+        catch (StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddAsync([FromBody] GroupForCreateDto dto, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.AddAsync(dto, cancellation);
+            return Ok(response);
+        }
+        catch (ValidationException ex)
+        {
+            return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+        }
+        catch (StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPut("{id:long}")]
+    public async Task<IActionResult> UpdateAsync([FromRoute] long id, [FromBody] GroupForUpdateDto dto, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.UpdateAsync(id, dto, cancellation);
+            return Ok(response);
+        }
+        catch (ValidationException ex)
+        {
+            return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+        }
+        catch (StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpDelete("{id:long}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute] long id, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.DeleteAsync(id, cancellation);
+            return Ok(response);
+        }
+        catch (StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpGet("{courseId:long}/course")]
+    public async Task<IActionResult> GetAllByCourseIdAsync([FromRoute] long courseId, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.GetAllByCourseIdAsync(courseId, cancellation);
+            return Ok(response);
+        }
+        catch (StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPost("filter")]
+    public async Task<IActionResult> FilterAsync([FromBody] GroupForFilterDto dto, CancellationToken cancellation = default)
+    {
+        try
+        {
+            var response = await _service.FilterAsync(dto, cancellation);
+            return Ok(response);
+        }
+        catch (StatusCodeException ex)
+        {
+            return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+}
