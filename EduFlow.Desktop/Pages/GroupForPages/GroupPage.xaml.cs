0d9d812f883786e81587b6ed0dd2eae208aaa8b1
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-04-11 22:40:57 +0500

    Enhance GroupPage with service integration and loading logic
    Updated `GroupPage.xaml` to include a `Loaded` event handler for initialization.
    Added necessary `using` directives in `GroupPage.xaml.cs` for group, course, and teacher services.
    Modified the constructor to initialize `_groupService`, `_courseService`, and `_teacherService`.
    Introduced a new `LoadPage` method to manage UI visibility based on user roles, called during the page load event.
    Removed previous constructor code to streamline initialization logic.

--- EduFlow.Desktop/Pages/GroupForPages/GroupPage.xaml.cs
+++ EduFlow.Desktop/Pages/GroupForPages/GroupPage.xaml.cs
@@ -1,28 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EduFlow.Desktop.Integrated.Security;
+using EduFlow.Desktop.Integrated.Services.Courses.Course;
+using EduFlow.Desktop.Integrated.Services.Courses.Group;
+using EduFlow.Desktop.Integrated.Services.Users.Teacher;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace EduFlow.Desktop.Pages.GroupForPages
+namespace EduFlow.Desktop.Pages.GroupForPages;
+
+/// <summary>
+/// Interaction logic for GroupPage.xaml
+/// </summary>
+public partial class GroupPage : Page
 {
-    /// <summary>
-    /// Interaction logic for GroupPage.xaml
-    /// </summary>
-    public partial class GroupPage : Page
+    private readonly IGroupService _groupService;
+    private readonly ICourseService _courseService;
+    private readonly ITeacherService _teacherService;
+    public GroupPage()
     {
-        public GroupPage()
+        InitializeComponent();
+        this._groupService = new GroupService();
+        this._courseService = new CourseService();
+        this._teacherService = new TeacherService();
+    }
+
+    private async Task LoadPage()
+    {
+        var id = IdentitySingelton.GetInstance().Id;
+        var role = IdentitySingelton.GetInstance().Role;
+
+        if(role == Domain.Enums.UserRole.Teacher)
         {
-            InitializeComponent();
+            courseComboBox.Visibility = Visibility.Collapsed;
+            teacherComboBox.Visibility = Visibility.Collapsed;
+            createGroupBtn.Visibility = Visibility.Collapsed;
         }
+        else
+        {
+
+        }
+    }
+
+    private void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+        LoadPage();
     }
 }
