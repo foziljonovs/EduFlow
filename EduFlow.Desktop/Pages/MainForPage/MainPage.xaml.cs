3addf3fc27e7825ec870a2eb3a765fc20c3b7f55
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-04-15 23:39:56 +0500

    Add course creation functionality and UI updates
    - Updated `createCourseBtn` in `MainPage.xaml` to include a `Click` event handler for opening the course creation window.
    - Implemented `createCourseBtn_Click` method in `MainPage.xaml.cs` to instantiate and display `CourseForCreateWindow`.
    - Modified `CategoryForCreateWindow.xaml` and `CourseForCreateWindow.xaml` to improve layout by removing unnecessary margins and adjusting title `TextBlock` properties.
    - Added a new `using` directive for `EduFlow.Desktop.Windows.CourseForWindows` in `MainPage.xaml.cs`.

--- EduFlow.Desktop/Pages/MainForPage/MainPage.xaml.cs
+++ EduFlow.Desktop/Pages/MainForPage/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using EduFlow.Desktop.Integrated.Services.Courses.Group;
 using EduFlow.Desktop.Integrated.Services.Users.Teacher;
 using EduFlow.Desktop.Windows;
 using EduFlow.Desktop.Windows.CategoryForWindows;
+using EduFlow.Desktop.Windows.CourseForWindows;
 using System.Windows;
 using System.Windows.Controls;
 using ToastNotifications;
@@ -329,6 +330,12 @@ public partial class MainPage : Page
     private void createCategoryBtn_Click(object sender, RoutedEventArgs e)
     {
         CategoryForCreateWindow window = new CategoryForCreateWindow();
+        window.ShowDialog();
+    }
+
+    private void createCourseBtn_Click(object sender, RoutedEventArgs e)
+    {
+        CourseForCreateWindow window = new CourseForCreateWindow();
         window.ShowDialog();
     }
 }
