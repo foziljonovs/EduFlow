31d94945a3ab529064b730057600657675908bc8
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-04-20 23:26:31 +0500

    Add StudentCourse mappings and validation logic
    This commit introduces new mappings and validation logic for the `StudentCourse` feature.
    Mappings for `StudentCourse` DTOs are added in `MappingProfile.cs`, enabling conversions between entities and DTOs.
    An interface `IStudentCourseValidator` is created to define validation methods for creating and updating `StudentCourse` DTOs.
    The `StudentCourseValidator` class implements this interface, and two new validator classes, `StudentCourseForCreateValidator` and `StudentCourseForUpdateValidator`, are added to enforce validation rules for `StudentId` and `CourseId`.

---
+++ EduFlow.BLL/Common/Validators/Courses/StudentCourse/StudentCourseForCreateValidator.cs
@@ -0,0 +1,18 @@
+﻿using EduFlow.BLL.DTOs.Courses.StudentCourse;
+using FluentValidation;
+
+namespace EduFlow.BLL.Common.Validators.Courses.StudentCourse;
+
+public class StudentCourseForCreateValidator : AbstractValidator<StudentCourseForCreateDto>
+{
+    public StudentCourseForCreateValidator()
+    {
+        RuleFor(x => x.StudentId)
+            .NotEmpty().WithMessage("Talaba Id si kiritish majburiy")
+            .GreaterThan(0).WithMessage("Talaba Id si 0 dan katta bo'lishi kerak");
+
+        RuleFor(x => x.CourseId)
+            .NotEmpty().WithMessage("Kurs Id si kiritish majburiy")
+            .GreaterThan(0).WithMessage("Kurs Id si 0 dan katta bo'lishi kerak");
+    }
+}
---
+++ EduFlow.BLL/Common/Validators/Courses/StudentCourse/StudentCourseForUpdateValidator.cs
@@ -0,0 +1,18 @@
+﻿using EduFlow.BLL.DTOs.Courses.StudentCourse;
+using FluentValidation;
+
+namespace EduFlow.BLL.Common.Validators.Courses.StudentCourse;
+
+public class StudentCourseForUpdateValidator : AbstractValidator<StudentCourseForUpdateDto>
+{
+    public StudentCourseForUpdateValidator()
+    {
+        RuleFor(x => x.StudentId)
+            .NotEmpty().WithMessage("Talaba Id si kiritish majburiy")
+            .GreaterThan(0).WithMessage("Talaba Id si 0 dan katta bo'lishi kerak");
+
+        RuleFor(x => x.CourseId)
+            .NotEmpty().WithMessage("Kurs Id si kiritish majburiy")
+            .GreaterThan(0).WithMessage("Kurs Id si 0 dan katta bo'lishi kerak");
+    }
+}
