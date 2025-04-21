72c746de60e4c82fce369c21e76974f1154237b8
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-04-21 17:14:53 +0500

    Add student course management interfaces and implementations
    Introduced `IStudentCourseServer` and `IStudentCourseService` interfaces for managing student courses. Implemented `StudentCourseServer` for API interactions and `StudentCourseService` as a service layer. Both classes include error handling for robust API communication.

---
+++ EduFlow.Desktop.Integrated/Servers/Interfaces/Courses/StudentCourse/IStudentCourseServer.cs
@@ -0,0 +1,13 @@
+﻿using EduFlow.BLL.DTOs.Courses.StudentCourse;
+
+namespace EduFlow.Desktop.Integrated.Servers.Interfaces.Courses.StudentCourse;
+
+public interface IStudentCourseServer
+{
+    Task<List<StudentCourseForResultDto>> GetAllAsync();
+    Task<StudentCourseForResultDto> GetByIdAsync(long id);
+    Task<bool> AddAsync(StudentCourseForCreateDto studentCourse);
+    Task<bool> UpdateAsync(long id, StudentCourseForUpdateDto studentCourse);
+    Task<bool> DeleteAsync(long id);
+    Task<bool> ExistsAsync(long id);
+}
