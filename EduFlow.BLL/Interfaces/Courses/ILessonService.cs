cb690c4fcffeebbc30ca265395f9c25a35f5f5fd
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-04-27 22:29:49 +0500

    Refactor attendance service and add lesson management
    - Updated `IAttendanceService` to include `UpdateRangeAsync` for batch updates and replaced `GetAllByGroupIdAsync` with `GetAllByLessonIdAsync`.
    - Modified `AttendanceService` to shift focus from courses to lessons, updating relevant logic and methods.
    - Introduced new `ILessonService` interface for managing lessons, including CRUD operations.

---
+++ EduFlow.BLL/Interfaces/Courses/ILessonService.cs
@@ -0,0 +1,12 @@
+﻿using EduFlow.BLL.DTOs.Courses.Lesson;
+
+namespace EduFlow.BLL.Interfaces.Courses;
+
+public interface ILessonService
+{
+    Task<IEnumerable<LessonForResultDto>> GetAllAsync(CancellationToken cancellation = default);
+    Task<LessonForResultDto> GetByIdAsync(long id, CancellationToken cancellation = default);
+    Task<bool> AddAsync(LessonForCreateDto lesson, CancellationToken cancellation = default);
+    Task<bool> UpdateAsync(LessonForUpdateDto lesson, CancellationToken cancellation = default);
+    Task<bool> DeleteAsync(long id, CancellationToken cancellation = default);
+}
