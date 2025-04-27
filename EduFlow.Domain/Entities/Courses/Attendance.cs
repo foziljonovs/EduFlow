5726b82e737b194173ab3532bff410d0bf6b8d9c
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-04-27 21:37:06 +0500

    Refactor attendance and lesson entity relationships
    Updated `BaseEntity.cs` to include schema attributes.
    Modified `Attendance.cs` to replace group references with lesson references.
    Introduced `Lesson.cs` with properties for lesson details and a relationship to attendance records.

--- EduFlow.Domain/Entities/Courses/Attendance.cs
+++ EduFlow.Domain/Entities/Courses/Attendance.cs
@@ -9,9 +9,9 @@ public class Attendance : BaseEntity
     [Column("student_id")]
     public required long StudentId { get; set; }
     public Student Student { get; set; }
-    [Column("group_id")]
-    public required long GroupId { get; set; }
-    public Group Group { get; set; }
+    [Column("lesson_id")]
+    public required long LessonId { get; set; }
+    public Lesson Lesson { get; set; }
     [Column("date")]
     public DateTime Date { get; set; }
     [Column("is_actived")]
