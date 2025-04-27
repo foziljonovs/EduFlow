c6f0e64dbfe5150f65d58672f0c02a8c904b81dd
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-04-27 22:41:51 +0500

    Add Lesson feature with DTOs and validation
    This commit introduces a new `Lesson` feature to the application, including the addition of DTOs for `Lesson` and validators for creating and updating lessons. The `MappingProfile` has been updated to include mappings for the new `Lesson` DTOs.
    
    Validation rules in `AttendanceForCreateValidator` and `AttendanceForUpdateValidator` have been modified to incorporate a new `LessonId` property, shifting focus from group-based to lesson-based attendance.
    
    A new interface `ILessonValidator` has been created, along with corresponding validator classes (`LessonForCreateValidator` and `LessonForUpdateValidator`) to enforce rules on the `Lesson` DTOs. The `LessonValidator` class implements this interface for asynchronous validation.
    
    Additionally, the `LessonService` class has been introduced to manage business logic related to lessons, including methods for adding, deleting, retrieving, and updating lessons, though the implementations are currently placeholders.

---
+++ EduFlow.BLL/Common/Validators/Courses/Services/LessonValidator.cs
@@ -0,0 +1,19 @@
+﻿using EduFlow.BLL.Common.Validators.Courses.Interfaces;
+using EduFlow.BLL.DTOs.Courses.Lesson;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace EduFlow.BLL.Common.Validators.Courses.Services;
+
+public class LessonValidator(
+    IValidator<LessonForCreateDto> craeteValidator,
+    IValidator<LessonForUpdateDto> updateValidator) : ILessonValidator
+{
+    private readonly IValidator<LessonForCreateDto> _createValidator = craeteValidator;
+    private readonly IValidator<LessonForUpdateDto> _updateValidator = updateValidator;
+    public async Task<ValidationResult> ValidateCraete(LessonForCreateDto dto)
+        => await _createValidator.ValidateAsync(dto);
+
+    public async Task<ValidationResult> ValidateUpdate(LessonForUpdateDto dto)
+        => await _updateValidator.ValidateAsync(dto);
+}
---
+++ EduFlow.BLL/Services/Courses/LessonService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using EduFlow.BLL.DTOs.Courses.Lesson;
+using EduFlow.BLL.Interfaces.Courses;
+using EduFlow.DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace EduFlow.BLL.Services.Courses;
+
+public class LessonService(
+    IUnitOfWork unitOfWork,
+    IMapper mapper,
+    ILogger<LessonService> logger) : ILessonService
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IMapper _mapper = mapper;
+    private readonly ILogger<LessonService> _logger = logger;
+    public Task<bool> AddAsync(LessonForCreateDto lesson, CancellationToken cancellation = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(long id, CancellationToken cancellation = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<LessonForResultDto>> GetAllAsync(CancellationToken cancellation = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LessonForResultDto> GetByIdAsync(long id, CancellationToken cancellation = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(LessonForUpdateDto lesson, CancellationToken cancellation = default)
+    {
+        throw new NotImplementedException();
+    }
+}
