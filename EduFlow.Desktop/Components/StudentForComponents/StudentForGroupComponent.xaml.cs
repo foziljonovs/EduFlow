cd9b774f0540bfc8e13e5d3a644ce95a97460e06
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-05-02 15:16:54 +0500

    Add StudentForGroupComponent UserControl
    Introduce a new UserControl `StudentForGroupComponent` to display student information, including number, full name, age, address, phone number, course, and status. Implement a `SetValues` method in the code-behind to dynamically populate the UI with data from a `StudentForResultDto` object.

---
+++ EduFlow.Desktop/Components/StudentForComponents/StudentForGroupComponent.xaml.cs
@@ -0,0 +1,28 @@
+﻿using EduFlow.BLL.DTOs.Users.Student;
+using EduFlow.Domain.Enums;
+using System.Windows.Controls;
+
+namespace EduFlow.Desktop.Components.StudentForComponents;
+
+/// <summary>
+/// Interaction logic for StudentForGroupComponent.xaml
+/// </summary>
+public partial class StudentForGroupComponent : UserControl
+{
+    private long Id { get; set; }
+    public StudentForGroupComponent()
+    {
+        InitializeComponent();
+    }
+
+    public void SetValues(int number, StudentForResultDto dto)
+    {
+        Id = dto.Id;
+        tbNumber.Text = number.ToString();
+        tbFullname.Text = dto.Fullname;
+        tbCourse.Text = dto.StudentCourses.Where(x => x.Status == EnrollmentStatus.Pending).FirstOrDefault().Course.Name ?? "Nomalum";
+        tbAge.Text = dto.Age.ToString();
+        tbAddress.Text = dto.Address;
+        tbPhoneNumber.Text = dto.PhoneNumber;
+    }
+}
