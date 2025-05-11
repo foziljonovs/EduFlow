e647b77b3611466bcb2ba315abbe71a5f7983f25
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-05-11 23:56:01 +0500

    Add GroupForMinComponent UserControl with data binding
    This commit introduces a new UserControl named `GroupForMinComponent` in the XAML file, featuring a layout with a border and a grid that includes multiple `TextBlock` elements. These elements are designed to display a number, name, teacher, student count, and status.
    
    The code-behind file is updated to define the partial class for `GroupForMinComponent`, which includes properties and methods for setting and retrieving displayed values. The `SetValues` method initializes the control with specific data, while the `GetId` method provides access to the component's unique identifier.

---
+++ EduFlow.Desktop/Components/GroupForComponents/GroupForMinComponent.xaml.cs
@@ -0,0 +1,30 @@
+﻿using EduFlow.Domain.Enums;
+using System.Windows.Controls;
+
+namespace EduFlow.Desktop.Components.GroupForComponents;
+
+/// <summary>
+/// Interaction logic for GroupForMinComponent.xaml
+/// </summary>
+public partial class GroupForMinComponent : UserControl
+{
+    private long Id { get; set; }
+    
+    public GroupForMinComponent()
+    {
+        InitializeComponent();
+    }
+
+    public void SetValues(int number, long id, string name, int studentCount, string teacherName, Status status)
+    {
+        this.Id = id;
+        tbNumber.Text = number.ToString();
+        tbName.Text = name;
+        tbStudentCount.Text = studentCount.ToString();
+        tbTeacher.Text = teacherName;
+        tbStatus.Text = status.ToString();
+    }
+
+    public long GetId()
+        => this.Id;
+}
