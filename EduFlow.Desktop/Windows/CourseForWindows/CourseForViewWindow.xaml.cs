b7a331790bdbaaa27f19e6b5b07d1bfbb319e14e
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-05-12 22:33:31 +0500

    Enhance Course Viewing Functionality
    Updated CourseForComponent to set CourseId in CourseForViewWindow.
    Introduced ICourseService for fetching course details, added notification handling, and implemented methods for displaying course, teacher, and group data.
    Default values are set in the UI when no course data is available for improved user experience.

--- EduFlow.Desktop/Windows/CourseForWindows/CourseForViewWindow.xaml.cs
+++ EduFlow.Desktop/Windows/CourseForWindows/CourseForViewWindow.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System.Windows;
+﻿using EduFlow.BLL.DTOs.Courses.Course;
+using EduFlow.Desktop.Integrated.Services.Courses.Course;
+using EduFlow.Domain.Entities.Courses;
+using EduFlow.Domain.Entities.Users;
+using System.Threading.Tasks;
+using System.Windows;
+using ToastNotifications;
+using ToastNotifications.Lifetime;
+using ToastNotifications.Messages;
+using ToastNotifications.Position;
 
 namespace EduFlow.Desktop.Windows.CourseForWindows;
 
@@ -7,10 +16,49 @@ namespace EduFlow.Desktop.Windows.CourseForWindows;
 /// </summary>
 public partial class CourseForViewWindow : Window
 {
+    private readonly ICourseService _courseService;
+    private long CourseId { get; set; }
     public CourseForViewWindow()
     {
         InitializeComponent();
+        this._courseService = new CourseService();
     }
+
+    Notifier notifier = new Notifier(cfg =>
+    {
+        cfg.PositionProvider = new WindowPositionProvider(
+            parentWindow: Application.Current.MainWindow,
+            corner: Corner.TopRight,
+            offsetX: 20,
+            offsetY: 20);
+
+        cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
+            notificationLifetime: TimeSpan.FromSeconds(3),
+            maximumNotificationCount: MaximumNotificationCount.FromCount(2));
+
+        cfg.Dispatcher = Application.Current.Dispatcher;
+
+        cfg.DisplayOptions.Width = 200;
+        cfg.DisplayOptions.TopMost = true;
+    });
+
+    Notifier notifierThis = new Notifier(cfg =>
+    {
+        cfg.PositionProvider = new WindowPositionProvider(
+            parentWindow: Application.Current.Windows.OfType<Window>().SingleOrDefault(x => x.IsActive),
+            corner: Corner.TopRight,
+            offsetX: 20,
+            offsetY: 20);
+
+        cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
+            notificationLifetime: TimeSpan.FromSeconds(3),
+            maximumNotificationCount: MaximumNotificationCount.FromCount(2));
+
+        cfg.Dispatcher = Application.Current.Dispatcher;
+
+        cfg.DisplayOptions.Width = 200;
+        cfg.DisplayOptions.TopMost = true;
+    });
 
     private void CloseBtn_Click(object sender, RoutedEventArgs e)
         => this.Close();
@@ -23,4 +71,80 @@ public partial class CourseForViewWindow : Window
 
     private void MinButton_Click(object sender, RoutedEventArgs e)
         => this.WindowState = WindowState.Minimized;
+
+    public void SetCourseId(long courseId)
+        => this.CourseId = courseId;
+
+    private async Task<CourseForResultDto> GetCourse()
+    {
+        var course = await _courseService.GetByIdAsync(this.CourseId);
+
+        if (course is not null)
+            return course;
+        else
+            return new CourseForResultDto();
+    }
+
+    private async Task ShowCourse()
+    {
+        var course = await GetCourse();
+
+        if(course is not null)
+        {
+            tbName.Content = course.Name;
+            tbPrice.Content = course.Price.ToString();
+            tbTerm.Content = course.Term.ToString();
+            tbStatus.Content = course.Archived.ToString();
+            tbCategory.Content = course.Category.Name;
+
+            ShowTeachers(course.Teachers);
+            ShowGroups(course.Groups);
+        }
+        else
+        {
+            notifierThis.ShowWarning("Kurs ma'lumotlarini yuklashda xatolik yuz berdi! qayta urining");
+            ThisIsEmpty();
+        }
+    }
+
+    private void ShowTeachers(List<Teacher> teachers)
+    {
+        if (teachers.Any())
+        {
+
+        }
+        else
+        {
+            stTeachers.Visibility = Visibility.Collapsed;
+            emptyDataForTeacher.Visibility = Visibility.Visible;
+        }
+    }
+
+    private void ShowGroups(List<Group> groups)
+    {
+        if (groups.Any())
+        {
+
+        }
+        else
+        {
+            stGroups.Visibility = Visibility.Collapsed;
+            emptyDataForGroup.Visibility = Visibility.Visible;
+        }
+    }
+
+    private void ThisIsEmpty()
+    {
+        tbName.Content = "Nomalum";
+        tbPrice.Content = "0";
+        tbTerm.Content = "0";
+        tbStatus.Content = "Nomalum";
+        tbCategory.Content = "Nomalum";
+
+        stGroups.Visibility = Visibility.Collapsed;
+        emptyDataForGroup.Visibility = Visibility.Visible;
+
+        stTeachers.Visibility = Visibility.Collapsed;
+        emptyDataForTeacher.Visibility = Visibility.Visible;
+    }
 }
