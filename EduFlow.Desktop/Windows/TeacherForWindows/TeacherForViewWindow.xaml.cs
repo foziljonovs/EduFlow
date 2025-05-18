a5eb13af19c032c9153e78f5f1ee31fb0bd098f4
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-05-18 16:18:52 +0500

    Add TeacherForViewWindow and refactor UI components
    This commit introduces a new XAML window (`TeacherForViewWindow.xaml`) along with its code-behind file (`TeacherForViewWindow.xaml.cs`) to display detailed teacher information, including name, phone number, skills, course, salary, and student count. The window includes controls for minimizing, maximizing, and closing. Additionally, minor formatting changes were made to `GroupForMinComponent.xaml` and `StudentForAttendanceComponent.xaml` for consistency in layout.

---
+++ EduFlow.Desktop/Windows/TeacherForWindows/TeacherForViewWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace EduFlow.Desktop.Windows.TeacherForWindows;
+
+/// <summary>
+/// Interaction logic for TeacherForViewWindow.xaml
+/// </summary>
+public partial class TeacherForViewWindow : Window
+{
+    public TeacherForViewWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        => this.Close();
+
+    private void MaxButton_Click(object sender, RoutedEventArgs e)
+        => this.WindowState = WindowState.Maximized;
+
+    private void NormalButton_Click(object sender, RoutedEventArgs e)
+        => this.WindowState = WindowState.Normal;
+
+    private void MinButton_Click(object sender, RoutedEventArgs e)
+        => this.WindowState = WindowState.Minimized;
+}
