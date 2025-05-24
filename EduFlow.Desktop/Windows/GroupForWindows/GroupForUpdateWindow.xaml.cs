b1e49d2de34cb6fea3dd44cfa91d7e79d35202d7
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-05-24 22:24:31 +0500

    Add GroupForUpdateWindow with UI and functionality
    This commit introduces a new WPF window named `GroupForUpdateWindow`. The XAML file defines the layout and UI elements, including a title, a close button, text blocks, text boxes, and combo boxes for selecting a course and a teacher. The window is styled with specific resources and is set to open in the center of the screen. The code-behind file contains the logic for the window, including a constructor that initializes the components and an event handler for the close button that closes the window when clicked.

---
+++ EduFlow.Desktop/Windows/GroupForWindows/GroupForUpdateWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace EduFlow.Desktop.Windows.GroupForWindows;
+
+/// <summary>
+/// Interaction logic for GroupForUpdateWindow.xaml
+/// </summary>
+public partial class GroupForUpdateWindow : Window
+{
+    public GroupForUpdateWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void closeBtn_Click(object sender, RoutedEventArgs e)
+        => this.Close();
+}
