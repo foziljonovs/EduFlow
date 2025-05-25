94f3c0c3a915d3b044221c3079033a0cb9dd0c55
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-05-25 23:15:36 +0500

    Add save functionality to StudentForUpdateWindow
    - Implemented Click event handler for save button.
    - Updated using directives to include StudentCourse namespace.
    - Added _studentCourseService for managing student courses.
    - Introduced SavedAsync method for input validation and updates.
    - Modified saveBtn_Click to call SavedAsync and manage button state.
    - Added user notifications for success and error feedback.

--- EduFlow.Desktop/Windows/StudentForWindows/StudentForUpdateWindow.xaml.cs
+++ EduFlow.Desktop/Windows/StudentForWindows/StudentForUpdateWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using EduFlow.BLL.DTOs.Users.Student;
+﻿using EduFlow.BLL.DTOs.Courses.StudentCourse;
+using EduFlow.BLL.DTOs.Users.Student;
 using EduFlow.Desktop.Integrated.Services.Courses.Course;
+using EduFlow.Desktop.Integrated.Services.Courses.StudentCourse;
 using EduFlow.Desktop.Integrated.Services.Users.Student;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using ToastNotifications;
@@ -17,6 +20,7 @@ public partial class StudentForUpdateWindow : Window
 {
     private readonly IStudentService _service;
     private readonly ICourseService _courseService;
+    private readonly IStudentCourseService _studentCourseService;
     private long Id { get; set; }
     private StudentForResultDto Student { get; set; } = new StudentForResultDto();
     public StudentForUpdateWindow()
@@ -24,6 +28,7 @@ public partial class StudentForUpdateWindow : Window
         InitializeComponent();
         this._service = new StudentService();
         this._courseService = new CourseService();
+        this._studentCourseService = new StudentCourseService();
     }
 
     Notifier notifier = new Notifier(cfg =>
@@ -162,4 +167,123 @@ public partial class StudentForUpdateWindow : Window
 
     private void closeBtn_Click(object sender, RoutedEventArgs e)
         => this.Close();
+
+    private async Task SavedAsync()
+    {
+        try
+        {
+            StudentForUpdateDto dto = new StudentForUpdateDto();
+
+            if (!string.IsNullOrEmpty(fullNameTxt.Text))
+                dto.Fullname = fullNameTxt.Text;
+            else
+            {
+                notifierThis.ShowWarning("Iltimos, to'liq ismingizni kiriting!");
+                fullNameTxt.Focus();
+                saveBtn.IsEnabled = true;
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(addressTxt.Text))
+                dto.Address = addressTxt.Text;
+            else
+            {
+                notifierThis.ShowWarning("Iltimos, manzilingizni kiriting!");
+                addressTxt.Focus();
+                saveBtn.IsEnabled = true;
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(phoneNumberTxt.Text) && phoneNumberTxt.Text.Length == 13)
+                dto.PhoneNumber = phoneNumberTxt.Text;
+            else
+            {
+                notifierThis.ShowWarning("Iltimos, telefon raqamingizni kiriting!");
+                phoneNumberTxt.Focus();
+                saveBtn.IsEnabled = true;
+                return;
+            }
+
+            if(AgeComboBox.SelectedItem is ComboBoxItem selectedItem && int.TryParse(selectedItem.Content.ToString(), out int age))
+                dto.Age = age;
+            else
+            {
+                notifierThis.ShowWarning("Iltimos, yoshingizni tanlang!");
+                AgeComboBox.Focus();
+                saveBtn.IsEnabled = true;
+                return;
+            }
+
+            var studentResult = await _service.UpdateAsync(this.Id, dto);
+
+            if (studentResult)
+            {
+                StudentCourseForUpdateDto studentCourseDto = new StudentCourseForUpdateDto()
+                {
+                    StudentId = this.Id
+                };
+
+                if (CourseComboBox.SelectedItem is ComboBoxItem selectedCourseItem && long.TryParse(selectedCourseItem.Tag.ToString(), out long courseId))
+                {
+                    studentCourseDto.CourseId = courseId;
+
+                    var updateStudentCourse = this.Student.StudentCourses.FirstOrDefault(x => x.CourseId == courseId
+                                        && x.Status == Domain.Enums.EnrollmentStatus.Active
+                                        || x.Status == Domain.Enums.EnrollmentStatus.Pending);
+
+                    var studentCourseResult = await _studentCourseService.UpdateAsync(updateStudentCourse.Id, studentCourseDto);
+
+                    if(studentCourseResult)
+                    {
+                        this.Close();
+                        notifier.ShowSuccess("Talaba malumotlari muvaffaqiyatli yangilandi!");
+                    }
+                    else
+                    {
+                        notifierThis.ShowWarning("Talaba kursini yangilashda xatolik yuz berdi!");
+                        saveBtn.IsEnabled = true;
+                        return;
+                    }
+                }
+                else
+                {
+                    notifierThis.ShowWarning("Iltimos, kursni tanlang!");
+                    CourseComboBox.Focus();
+                    saveBtn.IsEnabled = true;
+                    return;
+                }
+            }
+            else
+            {
+                notifierThis.ShowWarning("Talaba malumotlarini yangilashda xatolik yuz berdi!");
+                saveBtn.IsEnabled = true;
+                return;
+            }
+        }
+        catch(Exception ex)
+        {
+            notifierThis.ShowError("Xatolik yuz berdi, iltimos qayta urinib ko'ring!");
+            return;
+        }
+    }
+
+    private async void saveBtn_Click(object sender, RoutedEventArgs e)
+    {
+        if (!saveBtn.IsEnabled)
+        {
+            notifierThis.ShowWarning("Iltimos, kuting!");
+            return;
+        }
+
+        saveBtn.IsEnabled = false;
+
+        try
+        {
+            await SavedAsync();
+        }
+        finally
+        {
+            saveBtn.IsEnabled = true; 
+        }
+    }
 }
