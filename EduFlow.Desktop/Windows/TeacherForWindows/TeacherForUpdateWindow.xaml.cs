5c1cd8eb480c7bdd7f816b6d1b8c41c8faba1b27
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-05-25 16:02:43 +0500

    Add edit functionality for teacher details
    Implemented an "Edit" button in `TeacherForComponent.xaml` that opens a new `TeacherForUpdateWindow` for updating teacher information. The new window includes fields for full name, phone number, password, skills, and a course selection combo box. Logic for data retrieval and notifications has been added in `TeacherForUpdateWindow.xaml.cs`, enhancing the application's ability to manage teacher records.

---
+++ EduFlow.Desktop/Windows/TeacherForWindows/TeacherForUpdateWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using EduFlow.BLL.DTOs.Users.Teacher;
+using EduFlow.Desktop.Integrated.Services.Courses.Course;
+using EduFlow.Desktop.Integrated.Services.Users.Teacher;
+using System.Windows;
+using System.Windows.Controls;
+using ToastNotifications;
+using ToastNotifications.Lifetime;
+using ToastNotifications.Messages;
+using ToastNotifications.Position;
+
+namespace EduFlow.Desktop.Windows.TeacherForWindows;
+
+/// <summary>
+/// Interaction logic for TeacherForUpdateWindow.xaml
+/// </summary>
+public partial class TeacherForUpdateWindow : Window
+{
+    private readonly ITeacherService _service;
+    private readonly ICourseService _courseService;
+    private long Id { get; set; }
+    private TeacherForResultDto Teacher { get; set; } = new TeacherForResultDto();
+    public TeacherForUpdateWindow()
+    {
+        InitializeComponent();
+        this._service = new TeacherService();
+        this._courseService = new CourseService();
+    }
+
+    Notifier notifier = new Notifier(cfg =>
+    {
+        cfg.PositionProvider = new WindowPositionProvider(
+            parentWindow: Application.Current.Windows.OfType<Window>().FirstOrDefault(x => x.IsActive),
+            corner: Corner.TopRight,
+            offsetX: 50,
+            offsetY: 20);
+
+        cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
+            notificationLifetime: TimeSpan.FromSeconds(3),
+            maximumNotificationCount: MaximumNotificationCount.FromCount(2));
+
+        cfg.Dispatcher = Application.Current.Dispatcher;
+    });
+
+    Notifier notifierThis = new Notifier(cfg =>
+    {
+        cfg.PositionProvider = new WindowPositionProvider(
+            parentWindow: Application.Current.Windows.OfType<Window>().FirstOrDefault(x => x.IsActive),
+            corner: Corner.TopRight,
+            offsetX: 50,
+            offsetY: 20);
+
+        cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
+            notificationLifetime: TimeSpan.FromSeconds(3),
+            maximumNotificationCount: MaximumNotificationCount.FromCount(2));
+
+        cfg.Dispatcher = Application.Current.Dispatcher;
+    });
+
+    public void setId(long id)
+        => this.Id = id;
+
+    private async Task GetTeacher()
+    {
+        if (this.Id > 0)
+        {
+            var teacher = await _service.GetByIdAsync(this.Id);
+
+            if(teacher is not null)
+            {
+                this.Teacher = teacher;
+                fullNameTxt.Text = teacher.User.Firstname + " " + teacher.User.Lastname;
+                phoneNumberTxt.Text = teacher.User.PhoneNumber;
+                passwordTxt.Text = teacher.User.Password;
+                skillTxt.Text = string.Join(", ", teacher.Skills);
+            }
+        }
+        else
+            notifierThis.ShowError("Xatolik yuz berdi, iltimos qayta yuklang!");
+    }
+
+    private async Task GetAllCourse()
+    {
+        var courses = await Task.Run(async () => await _courseService.GetAllAsync());
+
+        if (courses.Any())
+        {
+            foreach (var course in courses)
+            {
+                ComboBoxItem item = new ComboBoxItem
+                {
+                    Tag = course.Id,
+                    Content = course.Name
+                };
+                courseComboBox.Items.Add(item);
+            }
+        }
+        else
+            notifierThis.ShowWarning("Kurslar topilmadi, iltimos qayta yuklang!");
+    }
+}
