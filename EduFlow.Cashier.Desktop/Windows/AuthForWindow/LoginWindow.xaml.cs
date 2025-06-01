fbb8dab80253663a2abc45e52df3b9457b2b8ed8
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-06-01 23:10:36 +0500

    Replace cashier.webp with cash_desk_07.jpg
    Deleted the binary file `cashier.webp` and added `cash_desk_07.jpg` as a new image resource. Updated `EduFlow.Cashier.Desktop.csproj` to manage the new asset correctly. Modified `LoginWindow.xaml` to use the new image and simplified `LoginWindow.xaml.cs` by removing unnecessary using directives and updating the constructor. Added a new method `CloseBtn_Click` to handle the close button functionality.

--- EduFlow.Cashier.Desktop/Windows/AuthForWindow/LoginWindow.xaml.cs
+++ EduFlow.Cashier.Desktop/Windows/AuthForWindow/LoginWindow.xaml.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace EduFlow.Cashier.Desktop.Windows.AuthForWindow
+namespace EduFlow.Cashier.Desktop.Windows.AuthForWindow;
+
+/// <summary>
+/// Interaction logic for LoginWindow.xaml
+/// </summary>
+public partial class LoginWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for LoginWindow.xaml
-    /// </summary>
-    public partial class LoginWindow : Window
+    public LoginWindow()
     {
-        public LoginWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
+
+    private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        => this.Close();
 }
