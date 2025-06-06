46149a3f684130b8d36d94aa5a3399939e075588
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-06-06 19:24:09 +0500

    Refactor payment DTOs to include discount and status
    Updated `PaymentForCreateDto`, `PaymentForResultDto`, and `PaymentForUpdateDto` to replace `Amount` with `Discount`. Added new properties: `Status`, `Type`, `ReceiptNumber`, and `Notes`. Made `StudentId`, `GroupId`, and `RegistryId` required in `PaymentForResultDto`. Included `using` directive for `EduFlow.Domain.Enums` to support new properties.

--- EduFlow.BLL/DTOs/Payments/Payment/PaymentForResultDto.cs
+++ EduFlow.BLL/DTOs/Payments/Payment/PaymentForResultDto.cs
@@ -1,6 +1,8 @@
 ﻿using EduFlow.Domain.Entities.Courses;
-using Et = EduFlow.Domain.Entities.Payments;
 using EduFlow.Domain.Entities.Users;
+using EduFlow.Domain.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+using Et = EduFlow.Domain.Entities.Payments;
 
 namespace EduFlow.BLL.DTOs.Payments.Payment;
 
@@ -9,12 +11,16 @@ public class PaymentForResultDto
     public long Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
-    public long StudentId { get; set; }
+    public required long StudentId { get; set; }
     public Student Student { get; set; }
-    public long GroupId { get; set; }
+    public required long GroupId { get; set; }
     public Group Group { get; set; }
-    public long RegistryId { get; set; }
+    public required long RegistryId { get; set; }
     public Et.Registry Registry { get; set; }
-    public double Amount { get; set; }
+    public double Discount { get; set; }
     public DateTime PaymentDate { get; set; }
+    public PaymentStatus Status { get; set; }
+    public PaymentType Type { get; set; }
+    public string? ReceiptNumber { get; set; }
+    public string? Notes { get; set; }
 }
--- EduFlow.BLL/DTOs/Payments/Payment/PaymentForUpdateDto.cs
+++ EduFlow.BLL/DTOs/Payments/Payment/PaymentForUpdateDto.cs
@@ -1,10 +1,16 @@
-﻿namespace EduFlow.BLL.DTOs.Payments.Payment;
+﻿using EduFlow.Domain.Enums;
+
+namespace EduFlow.BLL.DTOs.Payments.Payment;
 
 public class PaymentForUpdateDto
 {
     public long StudentId { get; set; }
     public long GroupId { get; set; }
     public long RegistryId { get; set; }
-    public double Amount { get; set; }
+    public double Discount { get; set; }
     public DateTime PaymentDate { get; set; }
+    public PaymentStatus Status { get; set; }
+    public PaymentType Type { get; set; }
+    public string? ReceiptNumber { get; set; }
+    public string? Notes { get; set; }
 }
