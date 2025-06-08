4504f3cee4698e0a5ffd2d80946bfb5450dc5f5c
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-06-08 23:21:34 +0500

    Add IPaymentService and IRegistryService interfaces
    Introduces two new interfaces: `IPaymentService` and `IRegistryService`.
    These interfaces provide asynchronous methods for managing payments and registries,
    including CRUD operations and specific queries for payments by student or group ID,
    as well as handling income and outlay for registries.

---
+++ EduFlow.Desktop.Integrated/Services/Payments/Registry/IRegistryService.cs
@@ -0,0 +1,11 @@
+﻿using EduFlow.BLL.DTOs.Payments.Registry;
+
+namespace EduFlow.Desktop.Integrated.Services.Payments.Registry;
+
+public interface IRegistryService
+{
+    Task<List<RegistryForResultDto>> GetAllAsync();
+    Task<RegistryForResultDto> GetByIdAsync(long id);
+    Task<long> IncomeAsync(RegistryForCreateDto dto);
+    Task<long> OutlayAsync(RegistryForCreateDto dto);
+}
