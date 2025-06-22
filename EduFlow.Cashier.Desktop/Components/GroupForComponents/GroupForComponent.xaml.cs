2e5301c790f0fcb0216a460af4e9f6e3f1c27c29
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-06-22 22:35:41 +0500

    Added components Group and Student for EduFlow.Cashier.Desktop

---
+++ EduFlow.Cashier.Desktop/Components/GroupForComponents/GroupForComponent.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+
+namespace EduFlow.Cashier.Desktop.Components.GroupForComponents;
+
+/// <summary>
+/// Interaction logic for GroupForComponent.xaml
+/// </summary>
+public partial class GroupForComponent : UserControl
+{
+    private long Id { get; set; }
+    public GroupForComponent()
+    {
+        InitializeComponent();
+    }
+
+    public void SetValues(long id, string name, int lessonCount, DateTime CreatedDate)
+    {
+        this.Id = id;
+        tbName.Text = name;
+        tbLessonCount.Text = lessonCount.ToString();
+        tbCreatedDate.Text = CreatedDate.ToString("dd/MM/yyyy");
+    }
+
+    public long GetId()
+        => this.Id;
+}
