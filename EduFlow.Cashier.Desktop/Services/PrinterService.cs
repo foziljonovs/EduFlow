377860437954a13dff0c044c7e3526d997243f3a
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-06-24 10:42:11 +0500

    Add printer settings and update project configuration
    Updated `EduFlow.Cashier.Desktop.csproj` to remove an asset and add package references for ESC-POS USB printing and Material Design themes. Created `App.config` for user settings, generated `Settings.Designer.cs` with a `PrinterName` setting, and added `Settings.settings` file. Introduced `PrinterService.cs` to handle printing functionality using the printer settings.

---
+++ EduFlow.Cashier.Desktop/Services/PrinterService.cs
@@ -0,0 +1,57 @@
+﻿using EduFlow.BLL.DTOs.Payments.Payment;
+using EduFlow.Desktop.Integrated.Security;
+using ESC_POS_USB_NET.Printer;
+using System.ComponentModel;
+using System.Text;
+
+namespace EduFlow.Cashier.Desktop.Services;
+
+public class PrinterService : IDisposable
+{
+    private readonly string PRINTER_NAME = Properties.Settings.Default.PrinterName;
+    public string printerName { get; set; } = string.Empty;
+    Printer? printer;
+    public PrinterService()
+    {
+        
+    }
+
+    public void Print(PaymentForResultDto paymentDto, double coursePrice)
+    {
+        var cashier = IdentitySingelton.GetInstance().Name;
+
+        printer = new Printer(PRINTER_NAME, "UTF-8");
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        printer.AlignCenter();
+        printer.DoubleWidth2();
+        printer.Append("Edu Flow");
+        printer.Separator();
+        printer.AlignLeft();
+        printer.Append($"Kurs to'lovi:      {coursePrice} so'm");
+        printer.Append($"To'lov turi:       {paymentDto.Type}");
+        printer.Append($"Chegirma:          {paymentDto.Discount} so'm");
+        printer.Append($"Jami summa:        {paymentDto.Amount + paymentDto.Discount} so'm");
+        printer.Append($"To'langan summa:   {paymentDto.Amount}");
+        printer.Append("\n");
+        printer.AlignLeft();
+        printer.Append($"Kassir: {cashier}");
+        printer.Append($"To'lov sanasi:     {paymentDto.PaymentDate:dd/MM/yyyy}");
+        printer.Append($"ID: {paymentDto.ReceiptNumber}");
+        printer.Append("\n");
+
+        printer.AlignCenter();
+        printer.BoldMode("Tashakkur!");
+
+        printer.Append("\n");
+
+        printer.FullPaperCut();
+        printer.PrintDocument();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
+
