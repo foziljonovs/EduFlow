0d50caa5a9fb1270ad20f17df85b4acfd095b213
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-06-28 22:48:40 +0500

    Added StudentService Filtering CourseStatus enum type

--- EduFlow.BLL/DTOs/Users/Student/StudentForFilterDto.cs
+++ EduFlow.BLL/DTOs/Users/Student/StudentForFilterDto.cs
@@ -1,8 +1,11 @@
-﻿namespace EduFlow.BLL.DTOs.Users.Student;
+﻿using EduFlow.Domain.Enums;
+
+namespace EduFlow.BLL.DTOs.Users.Student;
 
 public class StudentForFilterDto
 {
     public DateTime? StartedDate { get; set; }
     public DateTime? FinishedDate { get; set; }
     public long? CourseId { get; set; }
+    public EnrollmentStatus? CourseStatus { get; set; }
 }
