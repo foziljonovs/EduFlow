c86db6c815a954fcb1379ead9505390ae451174d
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-07-03 12:14:07 +0500

    Implement pagination in student retrieval methods
    Updated `IStudentService` and `StudentService` classes to return `PagedList<StudentForResultDto>` instead of `IEnumerable<StudentForResultDto>` for methods like `GetAllAsync`, `GetAllByCategoryIdAsync`, and `GetAllByCourseIdAsync`. Added `pageSize` and `pageNumber` parameters to support pagination. Adjusted method implementations to map student data to the new paged list format and included necessary using directives for pagination.

--- EduFlow.BLL/Interfaces/Users/IStudentService.cs
+++ EduFlow.BLL/Interfaces/Users/IStudentService.cs
@@ -1,11 +1,12 @@
-﻿using EduFlow.BLL.DTOs.Users.Student;
+﻿using EduFlow.BLL.Common.Pagination;
+using EduFlow.BLL.DTOs.Users.Student;
 using EduFlow.Domain.Enums;
 
 namespace EduFlow.BLL.Interfaces.Users;
 
 public interface IStudentService
 {
-    Task<IEnumerable<StudentForResultDto>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<PagedList<StudentForResultDto>> GetAllAsync(int pageSize, int pageNumber, CancellationToken cancellationToken = default);
     Task<StudentForResultDto> GetByIdAsync(long id, CancellationToken cancellationToken = default);
     Task<bool> AddAsync(StudentForCreateDto dto, CancellationToken cancellationToken = default);
     Task<long> AddAndReturnIdAsync(StudentForCreateDto dto, CancellationToken cancellationToken = default);
@@ -13,9 +14,9 @@ public interface IStudentService
     Task<bool> DeleteAsync(long id, CancellationToken cancellationToken = default);
     Task<StudentForResultDto> GetByPhoneNumberAsync(string phoneNumber, CancellationToken cancellationToken = default);
     Task<IEnumerable<StudentForResultDto>> GetAllByTeacherIdAsync(long teacherId, CancellationToken cancellationToken = default);
-    Task<IEnumerable<StudentForResultDto>> GetAllByCategoryIdAsync(long categoryId, CancellationToken cancellationToken = default); 
+    Task<PagedList<StudentForResultDto>> GetAllByCategoryIdAsync(long categoryId, int pageSize, int pageNumber, CancellationToken cancellationToken = default); 
     Task<bool> AddStudentByGroupAsync(long studentId, long groupId, CancellationToken cancellationToken = default);
     Task<bool> UpdateStudentByGroupAsync(long id, long groupId, Status status, CancellationToken cancellationToken = default);
-    Task<IEnumerable<StudentForResultDto>> GetAllByCourseIdAsync(long courseId, CancellationToken cancellationToken = default);
+    Task<PagedList<StudentForResultDto>> GetAllByCourseIdAsync(long courseId, int pageSize, int pageNumber, CancellationToken cancellationToken = default);
     Task<IEnumerable<StudentForResultDto>> FilterAsync(StudentForFilterDto dto, CancellationToken cancellation = default);
 }
