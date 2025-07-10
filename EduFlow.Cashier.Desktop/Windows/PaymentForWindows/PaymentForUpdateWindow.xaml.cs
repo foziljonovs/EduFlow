7ecc57fd6eea6a6df0725bb1468566ce397dd6c3
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-07-10 15:02:16 +0500

    Added GetPayment and GetAllTeacher PaymentForUpdateWindow

--- EduFlow.Cashier.Desktop/Windows/PaymentForWindows/PaymentForUpdateWindow.xaml.cs
+++ EduFlow.Cashier.Desktop/Windows/PaymentForWindows/PaymentForUpdateWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using EduFlow.BLL.DTOs.Payments.Payment;
+using EduFlow.BLL.DTOs.Users.Teacher;
 using EduFlow.Desktop.Integrated.Services.Payments.Payment;
+using EduFlow.Desktop.Integrated.Services.Users.Teacher;
 using System.Windows;
+using System.Windows.Controls;
 using ToastNotifications;
 using ToastNotifications.Lifetime;
 using ToastNotifications.Messages;
@@ -14,12 +17,14 @@ namespace EduFlow.Cashier.Desktop.Windows.PaymentForWindows;
 public partial class PaymentForUpdateWindow : Window
 {
     private readonly IPaymentService _paymentService;
+    private readonly ITeacherService _teacherService;
     private long Id { get; set; }
     private PaymentForResultDto payment = new PaymentForResultDto();
     public PaymentForUpdateWindow()
     {
         InitializeComponent();
         this._paymentService = new PaymentService();
+        this._teacherService = new TeacherService();
     }
 
     Notifier notifier = new Notifier(cfg =>
@@ -96,6 +101,20 @@ public partial class PaymentForUpdateWindow : Window
     private void MinButton_Click(object sender, RoutedEventArgs e)
         => this.WindowState = WindowState.Minimized;
 
+    private async Task GetAllTeacher()
+    {
+        try
+        {
+            var teachers = await Task.Run(async () => await _teacherService.GetAllAsync());
+
+            ShowTeachers(teachers);
+        }
+        catch(Exception ex)
+        {
+            notifierThis.ShowError("O'qituvchi malumotlarini yuklashda xatolik yuz berdi, Iltimos qayta urining!");
+        }
+    }
+
     private async Task GetPayment()
     {
         try
@@ -110,15 +129,70 @@ public partial class PaymentForUpdateWindow : Window
         }
     }
 
+    private void ShowTeachers(List<TeacherForResultDto> teachers)
+    {
+        teacherComboBox.Items.Clear();
+
+        if (teachers.Any())
+        {
+            foreach(var teacher in teachers)
+            {
+                ComboBoxItem item = new ComboBoxItem
+                {
+                    Content = $"{teacher.User.Firstname} {teacher.User.Lastname}",
+                    Tag = teacher.Id,
+                };
+
+                teacherComboBox.Items.Add(item);
+            }
+        }
+        else
+        {
+            notifierThis.ShowError("O'qituvchi malumotlari yuklab bo'lmadi, Iltimos qayta urining!");
+        }
+    }
+
+    public void SetId(long id)
+        => this.Id = id;
+
     private void ShowPayment(PaymentForResultDto payment)
     {
         if(payment is not null)
         {
+            this.payment = payment;
 
+            AmountTxt.Text = payment.Amount.ToString();
+            DiscountTxt.Text = payment.Discount.ToString();
+            NotesTxt.Text = payment.Notes?.ToString() ?? "...";
         }
         else
         {
+            notifierThis.ShowError("To'lov malumotlarini yuklashda xatolik yuz berdi, Iltimos qayta urining!");
+        }
+    }
 
+    private void ShowActivites()
+    {
+        if(payment is not null)
+        {
+            paymentTypeComboBox.SelectedItem = payment.Type switch
+            {
+                Domain.Enums.PaymentType.Cash => paymentTypeComboBox.Items[0],
+                Domain.Enums.PaymentType.Card => paymentTypeComboBox.Items[1],
+                Domain.Enums.PaymentType.Transfer => paymentTypeComboBox.Items[2],
+                Domain.Enums.PaymentType.Credit => paymentTypeComboBox.Items[3],
+                Domain.Enums.PaymentType.Other => paymentTypeComboBox.Items[4]
+            };
+
+            foreach (var item in teacherComboBox.Items)
+            {
+                if(item is ComboBoxItem comboboxItem && 
+                    comboboxItem.Tag?.ToString() == payment.TeacherId.ToString())
+                {
+                    teacherComboBox.SelectedItem = item;
+                    break;
+                }
+            }
         }
     }
 
@@ -127,8 +201,16 @@ public partial class PaymentForUpdateWindow : Window
 
     }
 
+    private async Task Load()
+    {
+        await GetAllTeacher();
+        await GetPayment();
+
+        ShowActivites();
+    }
+
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
-
+        Load();
     }
 }
