b6c2f550d4209271902e5d156da0062d73a872ac
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-07-23 11:45:58 +0500

    Update StudentCourse

---
+++ EduFLow.DAL/Migrations/20250723064346_UpdateStudentCourse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduFlow.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateStudentCourse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "preferred_days",
+                table: "StudentCourses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "preferred_time",
+                table: "StudentCourses",
+                type: "time without time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "preferred_days",
+                table: "StudentCourses");
+
+            migrationBuilder.DropColumn(
+                name: "preferred_time",
+                table: "StudentCourses");
+        }
+    }
+}
---
+++ EduFlow.Domain/Enums/Day.cs
@@ -0,0 +1,8 @@
+﻿namespace EduFlow.Domain.Enums;
+
+public enum Day
+{
+    None = 0,
+    ToqKunlar,
+    JuftKunlar
+}
